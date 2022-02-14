b52c8c9c2afe101e24ebb5b56573d19e18fc36cf
ruben <rubenvanstaden@gmail.com>
2022-02-14 11:57:55 -0500

    fix: great only one github and anyonsys config file

--- compare.sh
+++ compare.sh
@@ -3,14 +3,5 @@
 set -ex
 
 cp "$HOME/.gitconfig" dot_gitconfig
-
-cp "$HOME/local/github.com/.gitconfig" dot_github_gitconfig
-cp "$HOME/go/src/github.com/.gitconfig" dot_github_gitconfig
-cp "$HOME/py/src/github.com/.gitconfig" dot_github_gitconfig
-cp "$HOME/cpp/src/github.com/.gitconfig" dot_github_gitconfig
-cp "$HOME/julia/src/github.com/.gitconfig" dot_github_gitconfig 
-
-cp "$HOME/local/anyonsys.com/.gitconfig" dot_anyon_gitconfig
-cp "$HOME/go/src/anyonsys.com/.gitconfig" dot_anyon_gitconfig
-cp "$HOME/py/src/anyonsys.com/.gitconfig" dot_anyon_gitconfig
-cp "$HOME/cpp/src/anyonsys.com/.gitconfig" dot_anyon_gitconfig
+cp "$HOME/.gitconfig-github" dot_github_gitconfig
+cp "$HOME/.gitconfig-anyonsys" dot_anyon_gitconfig
